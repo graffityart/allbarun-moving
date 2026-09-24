edecd6aaf450a71f5d7e003e6a05813cbf7c9504
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-24 23:44:44 +0900

    Update all estimate inquiry links to dbdbdeep URL

--- lib/external-links.ts
+++ lib/external-links.ts
@@ -1,1 +1,1 @@
-export const ESTIMATE_INQUIRY_URL = "http://tnr.kr/ma/link.php?lncd=S00276387CC06043365K";
+export const ESTIMATE_INQUIRY_URL = "https://dbdbdeep.com/ma/link.php?lncd=S00276387CC06043365K";
