c7b296db34f40e23f8b5829c8ece9e32138b637d
조문국 <120075237+graffityart@users.noreply.github.com>
2026-09-24 23:43:07 +0900

    Update estimate inquiry affiliate link

--- lib/external-links.ts
+++ lib/external-links.ts
@@ -1,1 +1,1 @@
-export const ESTIMATE_INQUIRY_URL = "http://tnr.kr/ma/link.php?lncd=S00281081VC06024562X";
+export const ESTIMATE_INQUIRY_URL = "http://tnr.kr/ma/link.php?lncd=S00276387CC06043365K";
